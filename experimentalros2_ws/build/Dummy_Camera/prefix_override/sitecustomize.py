51845370085e7956aace2d57765ee91dc6de4ab8
Gabriel Bronfman <gabrielbronfman@MacBook-Pro-8.local>
2023-10-06 13:20:38 -0400

    Removed WS, kept package

--- experimentalros2_ws/build/Dummy_Camera/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/gabe/experimentalros2_ws/install/Dummy_Camera'
--- experimentalros2_ws/build/Dummy_Camera/share/Dummy_Camera/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/gabe/experimentalros2_ws/build/Dummy_Camera"
